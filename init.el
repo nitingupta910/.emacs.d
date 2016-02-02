dd17df355f4f33f1fe92f8fcfef555552e0713d1
Nitin Gupta <nitingupta910@gmail.com>
2016-02-02 11:34:26 -0800

    C++ style

--- init.el
+++ init.el
@@ -193,3 +193,23 @@
   :modes typescript-mode)
 (add-to-list 'flycheck-checkers 'tslint)
 
+
+;; recognize files ending with .h as c++ files
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; style I want to use in c++ mode
+;; (source: http://www.emacswiki.org/emacs/CPlusPlusMode)
+(c-add-style "my-style"
+             '("stroustrup"
+               (indent-tabs-mode . nil)        ; use spaces rather than tabs
+               (c-basic-offset . 2)            ; indent by four spaces
+               (c-offsets-alist . ((inline-open . 0)  ; custom indentation rules
+                                   (brace-list-open . 0)
+                                   (statement-case-open . +)))))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "my-style")        ; use my-style defined above
+  (auto-fill-mode)
+  (c-toggle-auto-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
