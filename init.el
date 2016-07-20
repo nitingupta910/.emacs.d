67275b004f9707618d49ee71a10917478846299f
Nitin Gupta <nitin.m.gupta@oracle.com>
2016-07-20 14:45:10 -0700

    Use linux kernel style for C files

--- init.el
+++ init.el
@@ -79,10 +79,43 @@
 
 
 ;; indentation (linux kernel style)
+;;(add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+;;               (setq c-default-style "linux"
+;;                     c-basic-offset 4))))
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
 (add-hook 'c-mode-common-hook
-           (lambda ()
-             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-               (setq c-default-style "linux"))))
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+                         (string-match (expand-file-name "~/src")
+                                       filename))
+                (setq indent-tabs-mode t)
+                (setq show-trailing-whitespace t)
+                (c-set-style "linux-tabs-only")))))
+
+;; Open header file in C mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
 
 ;;(setq c-default-style "linux")
 
