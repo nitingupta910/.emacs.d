becb28465852c8127f29a5b30f8117cce3fc6192
Nitin Gupta <nitin.m.gupta@oracle.com>
2016-07-29 17:57:44 -0700

    Elm autocomplete and flycheck work

--- init.el
+++ init.el
@@ -247,16 +247,19 @@
 (setq auto-save-default nil)
 
 ;;-----------------
-;; ELM
+;; Elm
 (require 'elm-mode)
 
 (add-hook 'flycheck-mode-hook 'flycheck-elm-setup)
 (add-hook 'elm-mode-hook
-          (lambda ()
-            (setq company-backends '(company-elm))))
-;            (set (make-local-variable 'company-backends) '(company-elm))))
+           (lambda ()
+             (set (make-local-variable 'company-backends) '(company-elm))
+             (company-mode)))
 
+;;(with-eval-after-load 'company
+;;  (add-to-list 'company-backends 'company-elm))
 (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+
 (setq elm-format-on-save t)
 
 ;; Org mode
