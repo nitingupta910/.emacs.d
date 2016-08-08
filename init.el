25786754420227b640f367980a75fd5bd817854a
Nitin Gupta <nitin.m.gupta@oracle.com>
2016-08-08 16:55:23 -0700

    Ediff split horizontally by default

--- init.el
+++ init.el
@@ -303,6 +303,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-merge-split-window-function (quote split-window-horizontally))
  '(org-babel-load-languages
    (quote
     ((sh . t)
