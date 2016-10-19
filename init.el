e20347f5c2cab75d31bcd51c3346e10e7d0c9a51
Nitin Gupta <nitingupta910@gmail.com>
2016-10-19 10:41:24 -0700

    Remove custom ediff colors
    monokai already defines ediff colors
    see: https://github.com/oneKelvinSmith/monokai-emacs/blob/master/monokai-theme.el

--- init.el
+++ init.el
@@ -471,8 +471,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ediff-current-diff-C ((t (:background "dark slate gray"))))
- '(ediff-even-diff-C ((t (:background "DeepSkyBlue4"))))
- '(ediff-odd-diff-A ((t (:background "dark olive green"))))
- '(ediff-odd-diff-B ((t (:background "dark olive green"))))
- '(ediff-odd-diff-C ((t (:background "dark olive green")))))
+ ;'(ediff-current-diff-C ((t (:background "dark slate gray"))))
+ ;'(ediff-even-diff-C ((t (:background "DeepSkyBlue4"))))
+ ;'(ediff-odd-diff-A ((t (:background "dark olive green"))))
+ ;'(ediff-odd-diff-B ((t (:background "dark olive green"))))
+ ;'(ediff-odd-diff-C ((t (:background "dark olive green"))))
+ )
