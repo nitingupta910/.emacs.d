0ca5c69e54e5625c632b51d31da815cfe59a86fc
Nitin Gupta <nitin.m.gupta@oracle.com>
2016-10-19 11:44:15 -0700

    Don't mess with default face fg color

--- init.el
+++ init.el
@@ -28,7 +28,8 @@
 (setq gruvbox-fg0 "#fbf1c7")
 (setq gruvbox-fg1 "#ebdbb2")
 (setq gruvbox-fg2 "#d5c4a1")
-(set-face-attribute 'default nil :height 180 :foreground gruvbox-fg1)
+(set-face-attribute 'default nil :height 180)
+;(set-face-attribute 'default nil :height 180 :foreground gruvbox-fg1)
 
 ;; We don't want to type yes and no all the time so, do y and n
 (defalias 'yes-or-no-p 'y-or-n-p)
