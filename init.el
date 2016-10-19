cc140dd5be9192182260eccab5e1b74eba2b4e5f
Nitin Gupta <nitingupta910@gmail.com>
2016-10-18 19:58:06 -0700

    Custom ediff colors

--- init.el
+++ init.el
@@ -466,3 +466,12 @@
  ;'(ediff-fine-diff-B ((t (:background "color-24"))))
  ;'(org-level-1 ((t (:inherit variable-pitch :foreground "tan" :height 1.0))))
  ;'(org-level-2 ((t (:inherit variable-pitch :foreground "#A6E22E" :height 1.0)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ediff-even-diff-C ((t (:background "DeepSkyBlue4"))))
+ '(ediff-odd-diff-A ((t (:background "dark olive green"))))
+ '(ediff-odd-diff-B ((t (:background "dark olive green"))))
+ '(ediff-odd-diff-C ((t (:background "dark olive green")))))
