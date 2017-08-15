a9f7ce2e1e1f1790eab53ccd17cc9d75d7165fae
Nitin Gupta <nitingupta910@gmail.com>
2017-08-15 14:18:39 -0700

    Always split window vertically

--- init.el
+++ init.el
@@ -501,9 +501,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-split-window-function (quote split-window-horizontally))
  '(package-selected-packages
    (quote
-    (helm use-package smooth-scrolling projectile popup helm-core))))
+	(helm use-package smooth-scrolling projectile popup helm-core)))
+ '(split-height-threshold 200)
+ '(split-width-threshold 0))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
