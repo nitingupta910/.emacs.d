45f646f5c88e07e3f37174d3d8d43433e7d4ed18
Nitin Gupta <nitingupta910@gmail.com>
2017-11-24 10:46:56 -0800

    set c++ indent style

--- init.el
+++ init.el
@@ -385,6 +385,21 @@
 (defun my-c++-mode-hook ()
   (local-set-key (kbd "C-d") 'mc/mark-next-like-this))
 
+(defun ng-cc-style()
+  (c-set-style "linux")
+  (c-set-offset 'innamespace '0)
+  (c-set-offset 'inextern-lang '0)
+  (c-set-offset 'inline-open '0)
+  (c-set-offset 'label '*)
+  (c-set-offset 'case-label '*)
+  (c-set-offset 'access-label '/)
+  (setq c-basic-offset 4)
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+)
+(add-hook 'c++-mode-hook 'ng-cc-style)
+
+
 (use-package helm-git-grep
   :ensure t
   :bind
