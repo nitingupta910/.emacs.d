4ace09e9e04a66a51ca7533fe9dce4ccee85e5fc
nitingupta910 <nitingupta910@gmail.com>
2018-06-15 14:59:32 -0700

    change lazy-highlight color

--- init.el
+++ init.el
@@ -592,5 +592,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lazy-highlight ((t (:inherit highlight :background "slate gray" :foreground "light gray")))))
 
